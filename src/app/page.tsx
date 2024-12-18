8b824dceff4ee89f8389b9805ee1f5d5267d202c
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-12-19 02:27:41 +0500

    key inserted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,26 @@
 import Link from "next/link";
 
 export default async function Home() {
-    const url= await fetch('https://jsonplaceholder.typicode.com/todos/', {
-      method: "Get"
-    })
-    const res= await url.json()
-    console.log(res)
+  const url = await fetch("https://jsonplaceholder.typicode.com/todos/", {
+    method: "GET",
+  });
+  const res = await url.json();
+  console.log(res);
 
   return (
     <main>
-    <h1 className="font-bold text-5xl">API Page</h1>
-    <br/>
+      <h1 className="font-bold text-5xl">API Page</h1>
+      <br />
 
-    {
-      res.map((todo: any) => (
-        <div>
+      {res.map((todo: any) => (
+        <div key={todo.id}>
           <Link href={`${todo.id}`}>
-          <h1>{todo.title} &nbsp; {todo.id}</h1>
+            <h1>
+              {todo.title} &nbsp; {todo.id}
+            </h1>
           </Link>
         </div>
-      )
-    )
-    }
+      ))}
     </main>
-  )
-
+  );
 }
